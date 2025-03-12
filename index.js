6ed8af7550cb731c556778ca67626bb21a2e06e0
Esther-C4275 <chieduesther02@gmail.com>
2025-03-12 17:36:18 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+//const button = document.querySelector("button");
+
+//<button onclick= "window.location.href='https://order.com'">order now</button>
+
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+
+// Get the current directory path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+    res.send("https://esther-c4275.github.io/resturant/")
+});
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, "exercise4")));
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
